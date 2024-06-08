6a97dce715094d8384fc4d522463f88b3668fdfa
sclwa <scleewa@outlook.com>
2024-06-09 08:42:33 +0900

    initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float thrustSpeed;
+    [SerializeField] float rotationSpeed;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();   
+    }
+
+    private void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddRelativeForce(thrustSpeed * Vector3.up * Time.deltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            ApplyRotation(rotationSpeed);
+        }
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    private void ApplyRotation(float rotationValue) 
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationValue * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
